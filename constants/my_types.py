a4192b57a4821e8857be1e58cef042f074aa48a1
MaxedOut4826 <maximreed@hotmail.com>
2026-05-15 01:06:32 +0100

    refactor: add new typings for intervals and objects

--- constants/my_types.py
+++ constants/my_types.py
@@ -1,4 +1,15 @@
-# from typing import TypedDict
+from typing import TypedDict, Callable
 
 Vector3 = list[int | float]
 Vector2 = list[int | float]
+
+
+class IntervalParameters(TypedDict):
+    callback: Callable[[], None]
+    interval: float
+    next: float
+
+
+class ObjectParameters(TypedDict):
+    vertices: list[Vector3]
+    indices: list[list[int]]
