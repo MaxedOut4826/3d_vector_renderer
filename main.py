39112c9be7b672d0312385166792f562eae4bc4a
MaxedOut4826 <maximreed@hotmail.com>
2026-05-21 20:16:00 +0100

    feat: add AI function to force UTF-8 in certain terminals like when using pypy for JIT speeds

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
-import sys
-from os import system
-from io import TextIOWrapper
-
-##########
-# import assets.index  # type: ignore
+from sys import stdout
+from os import system as run  # type: ignore
 import objects.index  # type: ignore
 import intervals.index  # type: ignore
-from constants.config import Ansi
-from intervals.intervals_manager import schedule_intervals
+from constants.ansi_codes import Ansi
 from render_pipeline.render_handler import Renderer
+from intervals.intervals_manager import schedule_intervals
+from utils.force_utf8 import force_utf8
 
 
 def main() -> None:
-    system("chcp 65001 > nul")
+    """
+    Forcing UTF-8 allows the special ASCII characters to show in the renderer for certain environments like pypy
+    """
+    force_utf8()
 
-    sys.stdout = TextIOWrapper(
-        sys.stdout.buffer, encoding="utf-8", errors="replace", line_buffering=True
-    )
+    stdout.write(f"{Ansi.hide_cursor}")
 
-    sys.stdout.write(f"{Ansi.hide_cursor}")
+    Renderer.get_screen_size()
     Renderer.clear_all()
+
     schedule_intervals()
 
 
---
+++ utils/force_utf8.py
@@ -0,0 +1,23 @@
+from os import system as run  # type: ignore
+import ctypes
+from ctypes import wintypes
+
+
+def force_utf8() -> None:
+    # AI solution for special ASCII characters not displaying to the terminal in pypy
+    # UTF-8 terminal
+    run("chcp 65001 > nul")
+
+    # Enable ANSI escape sequences
+    kernel32 = ctypes.windll.kernel32
+
+    STD_OUTPUT_HANDLE = -11
+    ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004
+
+    handle = kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+
+    mode = wintypes.DWORD()
+    kernel32.GetConsoleMode(handle, ctypes.byref(mode))
+
+    kernel32.SetConsoleMode(handle, mode.value | ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+    # -- End of AI code
