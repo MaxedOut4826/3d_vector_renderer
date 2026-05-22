71100a7904077933799e1a0c725f42ffef06996a
MaxedOut4826 <maximreed@hotmail.com>
2026-05-22 01:41:35 +0100

    perf: change camera move and rotate math to be manual instead of using Vector3Math calls

--- render_pipeline/camera.py
+++ render_pipeline/camera.py
@@ -1,4 +1,5 @@
-from . import Vector, Vector3
+from constants.config import FLOATING_POINT_PRECISION
+from . import Vector3
 
 
 class Camera:
@@ -8,20 +9,36 @@ class Camera:
     movement_speed: float = 1
     sensitivity: float = 1
 
+    """
+    This originally used Vector3Math to calculate but i didnt like the extra abstraction
+    This improves performance per action by about x6
+    """
+
     @staticmethod
     def move(movement_vector: Vector3) -> None:
-        Camera.position = Vector.round(
-            Vector.add(
-                Camera.position, Vector.multiply(movement_vector, Camera.movement_speed)
-            ),
-            4,
-        )
+
+        precision = FLOATING_POINT_PRECISION
+        speed = Camera.movement_speed
+
+        position_x, position_y, position_z = Camera.position
+        move_vector_x, move_vector_y, move_vector_z = movement_vector
+
+        Camera.position = [
+            round(position_x + move_vector_x * speed, precision),
+            round(position_y + move_vector_y * speed, precision),
+            round(position_z + move_vector_z * speed, precision),
+        ]
 
     @staticmethod
     def rotate(rotation_vector: Vector3) -> None:
-        Camera.rotation = Vector.round(
-            Vector.add(
-                Camera.rotation, Vector.multiply(rotation_vector, Camera.sensitivity)
-            ),
-            4,
-        )
+        precision = FLOATING_POINT_PRECISION
+        sensitivity = Camera.sensitivity
+
+        rotation_x, rotation_y, rotation_z = Camera.position
+        rotation_vector_x, rotation_vector_y, rotation_vector_z = rotation_vector
+
+        Camera.rotation = [
+            round(rotation_x + rotation_vector_x * sensitivity, precision),
+            round(rotation_y + rotation_vector_y * sensitivity, precision),
+            round(rotation_z + rotation_vector_z * sensitivity, precision),
+        ]
